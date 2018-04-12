5238494f3d9f31c85d694c38d965bde9305cb274
Armando <rivera.armando997@gmail.com>
2018-04-11 22:52:16 -0500

    Creacion del Main

---
+++ s-07-main.sql
@@ -0,0 +1,17 @@
+Prompt Proyecto Bases de Datos
+Prompt Presentado por:
+Prompt Rivera Negrete Manuel Armando
+Prompt Silva Lopez David Alejandro
+
+Prompt Creando Usuarios
+@s-01-usuarios.sql
+
+Prompt Creando objetos 
+@s-02-entidades.sql
+
+Prompt Carga de Datos
+@s-05-secuencia.sql
+@s-06-carga-inicial.sql
+
+Prompt Simulacion de un Carrito de compras con tablas temporales
+@s-03-tablas-temporales.sql
